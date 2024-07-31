21d47ef92bf8ac9eff2b5c2ef1442cfb87549dba
Pangtaek <zombie3632@gmail.com>
2024-07-31 17:43:56 +0900

    24262번: 알고리즘 수업 - 알고리즘의 수행 시간 1

---
+++ Baekjoon24262.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon24262 {
+    public static void main(String[] args) throws IOException {
+        new Baekjoon24262().answer();
+    }
+
+    public void answer() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(1);
+        System.out.println(0);
+
+        br.close();
+    }
+}
