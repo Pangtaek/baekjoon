3b97c9c4131fe93523662f8f842712072b9093c9
Pangtaek <zombie3632@gmail.com>
2025-02-10 16:36:09 +0900

    문자열 폭발

--- Baekjoon9935.java
+++ Baekjoon9935.java
@@ -1,52 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Baekjoon9935 {
-    public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            String str = br.readLine();
-            String boom = br.readLine();
-            StringBuilder answer = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String boom = br.readLine();
 
-            Stack<Character> stack = new Stack<>();
-
-            for (int i = 0; i < str.length(); i++) {
-                stack.push(str.charAt(i));
-
-                while (isBoom(stack, boom)) {
-                    for (int j = 0; j < boom.length(); j++) {
-                        stack.pop();
-                    }
-                }
-            }
-
-            if (stack.isEmpty()) {
-                System.out.println("FRULA");
-            } else {
-                for (char ch : stack) {
-                    answer.append(ch);
-                }
-                System.out.println(answer.toString());
-            }
-
-        } catch (IOException e) {
-            System.out.println("유근웅");
-        }
+        System.out.println(solution(str, boom));
     }
 
-    public static boolean isBoom(Stack<Character> stack, String boom) {
-        if (stack.size() < boom.length()) {
-            return false;
-        }
+    public static String solution(String str, String boom) {
+        StringBuilder sb = new StringBuilder();
+        int boomLen = boom.length();
 
-        for (int i = 0; i < boom.length(); i++) {
-            if (stack.get(stack.size() - 1 - i) != boom.charAt(boom.length() - 1 - i)) {
-                return false;
+        for (char c : str.toCharArray()) {
+            sb.append(c); // 문자열에 문자 추가
+
+            // 폭발 문자열이 나타났는지 확인
+            if (sb.length() >= boomLen && sb.substring(sb.length() - boomLen).equals(boom)) {
+                sb.setLength(sb.length() - boomLen); // 폭발 문자열 제거
             }
         }
 
-        return true;
+        return sb.length() == 0 ? "FRULA" : sb.toString();
     }
 }
