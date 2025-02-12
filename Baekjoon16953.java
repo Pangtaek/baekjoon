cfe3dfe04993fb6ffd0d74a3bb805e7f4d3fc2f6
Pangtaek <zombie3632@gmail.com>
2025-02-12 10:04:30 +0900

    A → B

--- Baekjoon16953.java
+++ Baekjoon16953.java
@@ -1,44 +1,52 @@
 import java.util.*;
 import java.io.*;
 
-public class Baekjoon16953 {
+public class Baekjoon16953{
+    public static Queue<Data> queue = new LinkedList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int[] input = Arrays.stream(br.readLine().split("\\s+"))
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
-        System.out.println(solution(input));
+        int start = input[0];
+        int end = input[1];
+
+        System.out.println(solution(start, end));
     }
 
-    public static int solution(int[] input) {
-        int start = input[0];
-        int target = input[1];
-
-        Queue<long[]> queue = new LinkedList<>();
-        queue.offer(new long[] { start, 1 }); // 시작 숫자, 연산 횟수
+    public static int solution(int start, int end) {
+        queue.offer(new Data((long) start, end, 1));
 
         while (!queue.isEmpty()) {
-            long[] curr = queue.poll();
-            long number = curr[0];
-            long count = curr[1];
+            Data current = queue.poll();
 
-            // 목표 값에 도달하면 연산 횟수 반환
-            if (number == target) {
-                return (int) count;
+            if (current.data == current.target) {
+                return (int) current.count;
             }
 
-            // 연산 수행: 2배
-            if (number * 2 <= target) {
-                queue.offer(new long[] { number * 2, count + 1 });
+            if (current.data * 2 <= end) {
+                queue.offer(new Data(current.data * 2, end, current.count + 1));
             }
-
-            // 연산 수행: 끝에 1 추가
-            if (number * 10 + 1 <= target) {
-                queue.offer(new long[] { number * 10 + 1, count + 1 });
+            if (current.data * 10 + 1 <= end) {
+                queue.offer(new Data(current.data * 10 + 1, end, current.count + 1));
             }
         }
 
-        return -1; // 목표 값에 도달할 수 없는 경우
+        return -1; // 목표 숫자로 도달할 수 없는 경우
+    }
+
+    public static class Data {
+        long data;
+        int target;
+        long count;
+
+        public Data(long data, int target, long count) {
+            this.data = data;
+            this.target = target;
+            this.count = count;
+        }
     }
 }
