73e3dbb912427defcef476dec863ea2e1a028039
Pangtaek <zombie3632@gmail.com>
2025-03-18 03:42:59 +0900

    N과 M (12)

--- Baekjoon15666.java
+++ Baekjoon15666.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 
 public class Baekjoon15666 {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] input = Arrays.stream(br.readLine().split("\\s+"))
                 .mapToInt(Integer::parseInt)
@@ -16,35 +15,29 @@ public class Baekjoon15666 {
                 .toArray();
 
         System.out.println(solution(input[0], input[1], data));
-
     }
 
     public static String solution(int N, int M, int[] data) {
-
         StringBuilder answer = new StringBuilder();
-
-        dfs(answer, 0, M, N, new int[M], data);
-
+        dfs(answer, 0, M, 0, new int[M], data);
         return answer.toString();
     }
 
-    public static void dfs(StringBuilder answer, int currDepth, int depth, int N, int[] arr, int[] data) {
-
+    public static void dfs(StringBuilder answer, int currDepth, int depth, int start, int[] arr, int[] data) {
         if (currDepth == depth) {
             for (int num : arr) {
                 answer.append(num).append(" ");
             }
             answer.append("\n");
-
             return;
         }
 
-        int prevNum = -1;
-        for (int i = 0; i < N; i++) {
+        int prevNum = Integer.MIN_VALUE;
+        for (int i = start; i < data.length; i++) {
             if (data[i] != prevNum) {
                 prevNum = data[i];
                 arr[currDepth] = data[i];
-                dfs(answer, currDepth + 1, depth, N, arr, data);
+                dfs(answer, currDepth + 1, depth, i, arr, data);
             }
         }
     }
