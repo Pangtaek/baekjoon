bb9655406fcd472b59351c1d12530398dbea653c
Pangtaek <zombie3632@gmail.com>
2025-03-24 01:31:06 +0900

    Gaaaaaaaaaarden

--- Baekjoon18809.java
+++ Baekjoon18809.java
@@ -11,86 +11,79 @@ public class Baekjoon18809 {
     public static int N, M, G, R;
     public static int[][] field;
     public static List<Position2D> availableSpots = new ArrayList<>();
-
     public static final int[] dx = { 1, -1, 0, 0 };
     public static final int[] dy = { 0, 0, 1, -1 };
 
-    public static final int EMPTY = 0;
-    public static final int GREEN = 1;
-    public static final int RED = 2;
-
-    public static int maxFlowers = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] input = Arrays.stream(br.readLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
         N = input[0];
         M = input[1];
         G = input[2];
         R = input[3];
 
         field = new int[N][M];
+
         for (int i = 0; i < N; i++) {
-            int[] row = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int[] row = Arrays.stream(br.readLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
             for (int j = 0; j < M; j++) {
                 field[i][j] = row[j];
-                if (row[j] == 2) {
+                if (field[i][j] == 2) {
                     availableSpots.add(new Position2D(j, i));
                 }
             }
         }
 
-        int[] selected = new int[availableSpots.size()];
-        dfs(0, 0, 0, selected);
-        System.out.println(maxFlowers);
+        Character[] selected = new Character[availableSpots.size()];
+        int result = dfs(0, 0, 0, selected, 0);
+        System.out.println(result);
     }
 
-    public static void dfs(int depth, int green, int red, int[] selected) {
+    public static int dfs(int depth, int green, int red, Character[] selected, int maximum) {
         if (green == G && red == R) {
-            maxFlowers = Math.max(maxFlowers, bfs(selected));
-            return;
+            return Math.max(maximum, bfs(selected));
         }
 
         if (depth == availableSpots.size())
-            return;
+            return maximum;
 
-        // 초록색 배양액 뿌리기
+        int result = maximum;
+
         if (green < G) {
-            selected[depth] = GREEN;
-            dfs(depth + 1, green + 1, red, selected);
-            selected[depth] = EMPTY;
+            selected[depth] = 'G';
+            result = Math.max(result, dfs(depth + 1, green + 1, red, selected, result));
+            selected[depth] = null;
         }
 
-        // 빨간색 배양액 뿌리기
         if (red < R) {
-            selected[depth] = RED;
-            dfs(depth + 1, green, red + 1, selected);
-            selected[depth] = EMPTY;
+            selected[depth] = 'R';
+            result = Math.max(result, dfs(depth + 1, green, red + 1, selected, result));
+            selected[depth] = null;
         }
 
-        // 아무것도 안 뿌리기
-        selected[depth] = EMPTY;
-        dfs(depth + 1, green, red, selected);
+        result = Math.max(result, dfs(depth + 1, green, red, selected, result));
+
+        return result;
     }
 
-    public static int bfs(int[] selected) {
+    public static int bfs(Character[] selected) {
         int[][] time = new int[N][M];
         char[][] color = new char[N][M];
-        Queue<Position2D> queue = new ArrayDeque<>();
+        Queue<Field> queue = new ArrayDeque<>();
 
         for (int i = 0; i < selected.length; i++) {
-            if (selected[i] != EMPTY) {
+            if (selected[i] != null) {
                 Position2D pos = availableSpots.get(i);
-                char c = selected[i] == GREEN ? 'G' : 'R';
-                color[pos.y][pos.x] = c;
+                color[pos.y][pos.x] = selected[i];
                 time[pos.y][pos.x] = 1;
-                queue.offer(new Position2D(pos.x, pos.y, c, 1));
+                queue.offer(new Field(pos.x, pos.y, 1, selected[i]));
             }
         }
 
         int flowers = 0;
         while (!queue.isEmpty()) {
-            Position2D curr = queue.poll();
+            Field curr = queue.poll();
 
             if (color[curr.y][curr.x] == 'F')
                 continue;
@@ -99,7 +92,7 @@ public class Baekjoon18809 {
                 int nx = curr.x + dx[d];
                 int ny = curr.y + dy[d];
 
-                if (!isInBound(nx, ny))
+                if (!isInRange(nx, ny))
                     continue;
                 if (field[ny][nx] == 0)
                     continue;
@@ -107,9 +100,8 @@ public class Baekjoon18809 {
                 if (color[ny][nx] == 0) {
                     color[ny][nx] = curr.color;
                     time[ny][nx] = curr.time + 1;
-                    queue.offer(new Position2D(nx, ny, curr.color, curr.time + 1));
-                } else if ((color[ny][nx] == 'G' || color[ny][nx] == 'R')
-                        && color[ny][nx] != curr.color
+                    queue.offer(new Field(nx, ny, curr.time + 1, curr.color));
+                } else if ((color[ny][nx] == 'G' || color[ny][nx] == 'R') && color[ny][nx] != curr.color
                         && time[ny][nx] == curr.time + 1) {
                     color[ny][nx] = 'F';
                     flowers++;
@@ -120,24 +112,27 @@ public class Baekjoon18809 {
         return flowers;
     }
 
-    public static boolean isInBound(int x, int y) {
+    public static boolean isInRange(int x, int y) {
         return 0 <= x && x < M && 0 <= y && y < N;
     }
 
     public static class Position2D {
-        int x, y, time;
-        char color;
+        public int x, y;
 
         public Position2D(int x, int y) {
             this.x = x;
             this.y = y;
         }
+    }
 
-        public Position2D(int x, int y, char color, int time) {
-            this.x = x;
-            this.y = y;
-            this.color = color;
+    public static class Field extends Position2D {
+        public int time;
+        public char color;
+
+        public Field(int x, int y, int time, char color) {
+            super(x, y);
             this.time = time;
+            this.color = color;
         }
     }
 }
