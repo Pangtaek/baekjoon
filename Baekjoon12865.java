4c453cf7fbcf2899b71fb5560df604bc93b2d815
Pangtaek <zombie3632@gmail.com>
2025-03-26 02:51:29 +0900

    평범한 배낭

--- Baekjoon12865.java
+++ Baekjoon12865.java
@@ -1,49 +1,33 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Baekjoon12865 {
-
-    static int N, K;
-    static int[] weight;
-    static int[] value;
-    static int maxValue = 0;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken()); // 물건 개수
-        K = Integer.parseInt(st.nextToken()); // 최대 무게
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        weight = new int[N];
-        value = new int[N];
+        // 무게마다 가치를 저장할 배열
+        int dp[] = new int[K + 1];
 
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            weight[i] = Integer.parseInt(st.nextToken());
-            value[i] = Integer.parseInt(st.nextToken());
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+
+            int W = Integer.parseInt(st2.nextToken());
+            int V = Integer.parseInt(st2.nextToken());
+
+            // 무게 뒤에서 부터 현재 해당 무게의 가치와
+            // 현재 물건을 더했을 때 해당 무게와 같아지는 가치 중
+            // 더 큰 가치를 해당 무게의 가치로 저장
+            for (int j = K; j >= W; j--) {
+                dp[j] = Math.max(dp[j], dp[j - W] + V);
+            }
         }
 
-        dfs(0, 0, 0); // 시작: index, 누적무게, 누적가치
-
-        System.out.println(maxValue);
-    }
-
-    public static void dfs(int index, int currentWeight, int currentValue) {
-        // 종료 조건: 모든 아이템을 고려한 경우
-        if (index == N) {
-            maxValue = Math.max(maxValue, currentValue);
-            return;
-        }
-
-        // 선택하지 않는 경우
-        dfs(index + 1, currentWeight, currentValue);
-
-        // 선택하는 경우 (무게 초과 체크)
-        if (currentWeight + weight[index] <= K) {
-            dfs(index + 1, currentWeight + weight[index], currentValue + value[index]);
-        }
+        System.out.println(dp[K]);
     }
 }
