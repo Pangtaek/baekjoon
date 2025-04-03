ab3d607500be579e3c3844c581bbd4c83a9f3020
Pangtaek <zombie3632@gmail.com>
2025-04-03 16:46:11 +0900

    치즈

--- Baekjoon2638.java
+++ Baekjoon2638.java
@@ -15,7 +15,6 @@ public class Baekjoon2638 {
 
     public static void main(String[] args) throws IOException {
         getInput();
-
         System.out.println(solution());
     }
 
@@ -28,83 +27,89 @@ public class Baekjoon2638 {
         M = input[1];
         cheeseBoard = new int[N][M];
 
-        for (int col = 0; col < N; col++) {
+        for (int i = 0; i < N; i++) {
             input = Arrays.stream(br.readLine().split("\\s+"))
                     .mapToInt(Integer::parseInt)
                     .toArray();
-            cheeseBoard[col] = input;
+            cheeseBoard[i] = input;
         }
     }
 
     public static int solution() {
-        int answer = 0;
+        int time = 0;
 
         while (true) {
-            if (getChessSize() == 0)
+            if (getCheeseCount() == 0)
                 break;
 
-            // 1. 외각 치즈 중 녹는 부분 설정
-            for (int col = 0; col < N; col++) {
-                for (int row = 0; row < M; row++) {
-                    if (cheeseBoard[col][row] == 1)
-                        bfs(new Position2D(row, col));
-                }
-            }
-
-            // 2. 치즈 녹임
+            markOutsideAir();
             meltCheese();
-
-            // 3. 시간 추가
-            answer++;
+            time++;
         }
 
-        return answer;
+        return time;
     }
 
-    public static void meltCheese() {
-        for (int col = 0; col < N; col++) {
-            for (int row = 0; row < M; row++) {
-                if (cheeseBoard[col][row] == 2) {
-                    cheeseBoard[col][row] = 0;
-                }
-            }
-        }
-    }
-
-    public static int getChessSize() {
+    public static int getCheeseCount() {
         int count = 0;
-
-        for (int col = 0; col < N; col++) {
-            for (int row = 0; row < M; row++) {
-                if (cheeseBoard[col][row] == 1)
+        for (int[] row : cheeseBoard) {
+            for (int val : row) {
+                if (val == 1)
                     count++;
             }
         }
-
         return count;
     }
 
-    public static void bfs(Position2D position) {
-        Queue<Position2D> queue = new ArrayDeque<>();
-        int[][] count = new int[N][M];
+    public static void markOutsideAir() {
+        visited = new boolean[N][M];
+        Queue<Position2D> q = new ArrayDeque<>();
+        q.offer(new Position2D(0, 0));
+        visited[0][0] = true;
 
-        queue.offer(position);
-
-        while (!queue.isEmpty()) {
-            Position2D curr = queue.poll();
+        while (!q.isEmpty()) {
+            Position2D curr = q.poll();
 
             for (int d = 0; d < 4; d++) {
-                int nx = curr.x = dx[d];
-                int ny = curr.y = dy[d];
+                int nx = curr.x + dx[d];
+                int ny = curr.y + dy[d];
 
-                boolean isAir = isInBounds(nx, ny) && cheeseBoard[ny][nx] == 0;
-                if (isAir) {
-                    count[curr.y][curr.x]++;
+                if (isInBounds(nx, ny) && !visited[ny][nx] && cheeseBoard[ny][nx] == 0) {
+                    visited[ny][nx] = true;
+                    q.offer(new Position2D(nx, ny));
                 }
             }
-            
-            if (count[curr.y][curr.x] == 2) {
-                cheeseBoard[curr.y][curr.x] = 2;
+        }
+    }
+
+    public static void meltCheese() {
+        for (int y = 0; y < N; y++) {
+            for (int x = 0; x < M; x++) {
+                if (cheeseBoard[y][x] == 1) {
+                    int contact = 0;
+
+                    for (int d = 0; d < 4; d++) {
+                        int nx = x + dx[d];
+                        int ny = y + dy[d];
+
+                        if (isInBounds(nx, ny) && visited[ny][nx] && cheeseBoard[ny][nx] == 0) {
+                            contact++;
+                        }
+                    }
+
+                    if (contact >= 2) {
+                        cheeseBoard[y][x] = 2; // 녹을 치즈 표시
+                    }
+                }
+            }
+        }
+
+        // 실제로 치즈 녹이기
+        for (int y = 0; y < N; y++) {
+            for (int x = 0; x < M; x++) {
+                if (cheeseBoard[y][x] == 2) {
+                    cheeseBoard[y][x] = 0;
+                }
             }
         }
     }
@@ -114,8 +119,7 @@ public class Baekjoon2638 {
     }
 
     public static class Position2D {
-        public int x;
-        public int y;
+        public int x, y;
 
         public Position2D(int x, int y) {
             this.x = x;
