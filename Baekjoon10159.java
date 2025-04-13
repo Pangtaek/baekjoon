566c54f9f85e132dec83bd1c18e68bcfda4c1334
Pangtaek <zombie3632@gmail.com>
2025-04-13 17:14:03 +0900

    저울

--- Baekjoon10159.java
+++ Baekjoon10159.java
@@ -4,8 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Baekjoon10159 {
-    public static int N; // 서로 다른 물건의 개수(1~N)
-    public static int M; // 미리 측정된 물건 쌍의 개수
+    public static int N, M;
     public static int[][] distance;
 
     public static void main(String[] args) throws IOException {
@@ -16,34 +15,35 @@ public class Baekjoon10159 {
 
         distance = new int[N + 1][N + 1];
 
-        for (int i = 0; i <= N; i++) {
+        for (int i = 1; i <= N; i++) {
             Arrays.fill(distance[i], Integer.MAX_VALUE);
             distance[i][i] = 0;
         }
 
         for (int i = 0; i < M; i++) {
             int[] tokens = Arrays.stream(br.readLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
-
-            // obj1.weight > obj2.weight
-            int obj1 = tokens[0];
-            int obj2 = tokens[1];
-
-            distance[obj2][obj1] = 1;
+            int heavier = tokens[0]; // a > b
+            int lighter = tokens[1];
+            distance[heavier][lighter] = 1;
         }
 
+        // Floyd-Warshall
         for (int k = 1; k <= N; k++)
             for (int i = 1; i <= N; i++)
                 for (int j = 1; j <= N; j++)
-                    if (distance[i][k] + distance[k][j] < distance[i][j])
-                        distance[i][j] = distance[i][k] + distance[k][j];
+                    if (distance[i][k] != Integer.MAX_VALUE && distance[k][j] != Integer.MAX_VALUE)
+                        distance[i][j] = Math.min(distance[i][j], distance[i][k] + distance[k][j]);
 
+        // 정답 출력
         for (int i = 1; i <= N; i++) {
-            int count = 0;
+            int unknown = 0;
             for (int j = 1; j <= N; j++) {
-                if (distance[i][j] == Integer.MAX_VALUE)
-                    count++;
+                if (i == j)
+                    continue;
+                if (distance[i][j] == Integer.MAX_VALUE && distance[j][i] == Integer.MAX_VALUE)
+                    unknown++;
             }
-            System.out.println(count);
+            System.out.println(unknown);
         }
     }
 }
