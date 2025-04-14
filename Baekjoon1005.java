c1ac855e25922e7688d3ba2ed20fd6daf397cb52
Pangtaek <zombie3632@gmail.com>
2025-04-14 17:54:44 +0900

    ACM Craft

--- Baekjoon1005.java
+++ Baekjoon1005.java
@@ -10,19 +10,22 @@ import java.util.Queue;
 public class Baekjoon1005 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int T = Integer.parseInt(br.readLine());
-        int[] tokens;
+
         while (T-- > 0) {
-            tokens = Arrays.stream(br.readLine().split("\\s+"))
+            int[] tokens = Arrays.stream(br.readLine().split("\\s+"))
                     .mapToInt(Integer::parseInt)
                     .toArray();
             int N = tokens[0];
             int K = tokens[1];
 
-            int[] times = Arrays.stream(br.readLine().split("\\s+"))
+            int[] times = new int[N + 1];
+            tokens = Arrays.stream(br.readLine().split("\\s+"))
                     .mapToInt(Integer::parseInt)
                     .toArray();
+            for (int i = 1; i <= N; i++) {
+                times[i] = tokens[i - 1];
+            }
 
             List<List<Integer>> graph = new ArrayList<>();
             int[] indegrees = new int[N + 1];
@@ -37,14 +40,13 @@ public class Baekjoon1005 {
                         .toArray();
                 int from = tokens[0];
                 int to = tokens[1];
-
                 graph.get(from).add(to);
                 indegrees[to]++;
             }
 
             Queue<Integer> queue = new ArrayDeque<>();
             for (int i = 1; i <= N; i++) {
-                dp[i] = times[i];
+                dp[i] = times[i]; // 초기값: 자기 자신 건물 짓는 시간
                 if (indegrees[i] == 0)
                     queue.offer(i);
             }
@@ -55,11 +57,11 @@ public class Baekjoon1005 {
                 for (int next : graph.get(current)) {
                     dp[next] = Math.max(dp[next], dp[current] + times[next]);
                     indegrees[next]--;
-
                     if (indegrees[next] == 0)
                         queue.offer(next);
                 }
             }
+
             int W = Integer.parseInt(br.readLine());
             System.out.println(dp[W]);
         }
