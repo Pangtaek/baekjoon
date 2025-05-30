cd16a4885b9becc14cd90249592d2963a4f5af36
Pangtaek <zombie3632@gmail.com>
2025-05-31 02:24:10 +0900

    구간 합 구하기 5

--- Baekjoon11660.java
+++ Baekjoon11660.java
@@ -15,29 +15,18 @@ public class Baekjoon11660 {
                 .toArray();
         int N = tokens[0]; // 표의 크기
         int M = tokens[1]; // 합을 구해야 하는 횟수
-        int[][] table = new int[N][N];
+        int[][] table = new int[N + 1][N + 1]; // 1-indexed
         int[][] dp = new int[N + 1][N + 1];
 
-        for (int row = 0; row < N; row++) {
-            table[row] = Arrays.stream(br.readLine().split("\\s+"))
+        for (int row = 1; row <= N; row++) {
+            table[row] = new int[N + 1];
+            tokens = Arrays.stream(br.readLine().split("\\s+"))
                     .mapToInt(Integer::parseInt)
                     .toArray();
-        }
-
-        // 초기값 설정: 1행
-        for (int i = 1; i <= N; i++) {
-            dp[1][i] = dp[1][i - 1] + table[0][i - 1];
-        }
-
-        // 초기값 설정: 1열
-        for (int row = 2; row <= N; row++) {
-            dp[row][1] = dp[row - 1][1] + table[row - 1][0];
-        }
-
-        // DP 계산
-        for (int row = 2; row <= N; row++) {
-            for (int col = 2; col <= N; col++) {
-                dp[row][col] = dp[row][col - 1] + dp[row - 1][col];
+            for (int col = 1; col <= N; col++) {
+                table[row][col] = tokens[col - 1];
+                dp[row][col] = dp[row - 1][col] + dp[row][col - 1]
+                        - dp[row - 1][col - 1] + table[row][col];
             }
         }
 
@@ -50,7 +39,11 @@ public class Baekjoon11660 {
             int x2 = tokens[2];
             int y2 = tokens[3];
 
-            int result = dp[x2][y2] - dp[x1 - 1][y1 - 1];
+            int result = dp[x2][y2] // 전체 영역
+                    - dp[x1 - 1][y2] // 위쪽 제외
+                    - dp[x2][y1 - 1] // 왼쪽 제외
+                    + dp[x1 - 1][y1 - 1]; // 중복 제거 (위+왼쪽 교차 영역) 
+
             bw.write(Integer.toString(result));
             bw.newLine();
         }
