5548ce908271a75a3f76ce5b5e77f57233870f44
Pangtaek <zombie3632@gmail.com>
2025-06-01 02:29:22 +0900

    연속합

---
+++ Baekjoon1912.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Baekjoon1912 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int[] dp = new int[N];
+        dp[0] = arr[0];
+        int max = dp[0];
+
+        for (int i = 1; i < N; i++) {
+            dp[i] = Math.max(arr[i], dp[i - 1] + arr[i]);
+            max = Math.max(max, dp[i]);
+        }
+
+        bw.write(Integer.toString(max));
+        bw.newLine();
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
