0e35bf40a62a81fdd35fb62c8a797bd57c117da3
Pangtaek <zombie3632@gmail.com>
2025-06-03 02:16:22 +0900

    스타트와 링크

--- Baekjoon14889.java
+++ Baekjoon14889.java
@@ -1,68 +1,69 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Baekjoon14889 {
-    static int[][] board;
-    static boolean[] visited;
+    static int answer = Integer.MAX_VALUE;
     static int N;
-    static int Min = Integer.MAX_VALUE;
+    static int[][] S;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         N = Integer.parseInt(br.readLine());
-
-        board = new int[N][N];
-        visited = new boolean[N];
+        S = new int[N][N];
 
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
+            S[i] = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
         }
 
-        dfs(0, 0);
+        boolean[] selected = new boolean[N];
+        dfs(selected, 0, 0);
 
-        System.out.println(Min);
+        bw.write(Integer.toString(answer));
+        bw.newLine();
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
-    public static void dfs(int idx, int depth) {
+    static void dfs(boolean[] selected, int depth, int index) {
         if (depth == N / 2) {
-            int startTeam = 0, linkTeam = 0;
-
-            for (int i = 0; i < N - 1; i++) {
-                for (int j = i + 1; j < N; j++) { // 중복 방지
-                    if (visited[i] && visited[j]) {
-                        startTeam += board[i][j];
-                        startTeam += board[j][i];
-                    } else if (!visited[i] && !visited[j]) {
-                        linkTeam += board[i][j];
-                        linkTeam += board[j][i];
-                    }
-                }
-            }
-
-            // 두 팀의 점수 차이 (절댓값)
-            int val = Math.abs(startTeam - linkTeam);
-
-            if (val == 0) {
-                System.out.println(val);
-                System.exit(0);
-            }
-
-            Min = Math.min(val, Min);
+            int diff = calculateDifference(selected);
+            answer = Math.min(answer, diff);
             return;
         }
 
-        for (int i = idx; i < N; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                dfs(i + 1, depth + 1);
-                visited[i] = false;
+        for (int i = index; i < N; i++) {
+            if (!selected[i]) {
+                selected[i] = true;
+                dfs(selected, depth + 1, i + 1);
+                selected[i] = false;
             }
         }
+    }
+
+    static int calculateDifference(boolean[] selected) {
+        int start = 0;
+        int link = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (selected[i] && selected[j]) {
+                    start += S[i][j];
+                } else if (!selected[i] && !selected[j]) {
+                    link += S[i][j];
+                }
+            }
+        }
+
+        return Math.abs(start - link);
     }
 }
