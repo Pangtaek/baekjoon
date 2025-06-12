01743b046548b46a5aa344d9a23c2775eec52d8c
Pangtaek <zombie3632@gmail.com>
2025-06-12 19:12:50 +0900

    부분수열의 합

--- Baekjoon1182.java
+++ Baekjoon1182.java
@@ -1,36 +1,56 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
 public class Baekjoon1182 {
 
+    static int N;
+    static int S;
+    static int[] A;
+    static int count = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] input = Arrays.stream(br.readLine().split("\\s+"))
+        int[] tokens = Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
-        int[] arr = Arrays.stream(br.readLine().split("\\s+"))
+        N = tokens[0];
+        S = tokens[1];
+        A = Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
-        int answer = dfs(0, 0, arr, input[0], input[1]);
+        dfs(0, 0); // 부분수열 탐색 시작
 
-        // 공집합 제외 처리 (S가 0일 경우, 아무것도 선택하지 않은 경우 포함됨)
-        if (input[1] == 0)
-            answer--;
+        // S가 0인 경우, 공집합도 포함되므로 제외해야 함
+        if (S == 0)
+            count--;
 
-        System.out.println(answer);
+        bw.write(Integer.toString(count));
+        bw.newLine();
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
-    public static int dfs(int index, int sum, int[] arr, int N, int S) {
+    private static void dfs(int index, int sum) {
         if (index == N) {
-            return sum == S ? 1 : 0;
+            if (sum == S) {
+                count++;
+            }
+            return;
         }
 
-        // 현재 원소를 선택한 경우 + 선택하지 않은 경우의 합을 리턴
-        return dfs(index + 1, sum + arr[index], arr, N, S)
-                + dfs(index + 1, sum, arr, N, S);
+        // 현재 index 값을 포함
+        dfs(index + 1, sum + A[index]);
+
+        // 현재 index 값을 포함하지 않음
+        dfs(index + 1, sum);
     }
 }
