bdcc0a05e44ea2e0ee2c12669e698655ff6709ea
Pangtaek <zombie3632@gmail.com>
2025-06-24 23:22:06 +0900

    등록

---
+++ Baekjoon7287.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Baekjoon7287 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("297");
+        bw.newLine();
+        bw.write("pangtaek00");
+        bw.newLine();
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
