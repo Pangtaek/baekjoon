85a726c47e1cf4a385ca53bf49b93a87fb75a1d4
Pangtaek <zombie3632@gmail.com>
2025-07-15 23:29:58 +0900

    정ㅋ벅ㅋ

---
+++ Baekjoon1237.java
@@ -0,0 +1,16 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Baekjoon1237 {
+    
+    public static void main(String[] args) throws IOException{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("문제의 정답");
+        bw.newLine();
+
+        bw.flush();
+        bw.close();
+    }
+}
