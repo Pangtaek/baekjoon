8454a61d6faf0efb17eabaec0e3d5bcb8362d7c0
Pangtaek <zombie3632@gmail.com>
2025-12-08 23:49:00 +0900

    플로이드

--- Baekjoon11404.java
+++ Baekjoon11404.java
@@ -1,46 +1,68 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
 public class Baekjoon11404 {
+
     public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+
             int n = Integer.parseInt(br.readLine()); // 도시의 개수
             int m = Integer.parseInt(br.readLine()); // 버스의 개수
 
-            int[][] priceList = new int[n][n];
-            // 그래프 초기화 (모든 경로를 INF로 설정)
-            for (int i = 0; i < n; i++) {
-                Arrays.fill(priceList[i], Integer.MAX_VALUE);
-            priceList[i][i] = 0; // 자기 자신으로 가는 비용은 0
+            int[][] distance = new int[n][n];
+            for (int city = 0; city < n; city++) {
+                Arrays.fill(distance[city], Integer.MAX_VALUE);
+                distance[city][city] = 0;
             }
-            for (int i = 0; i < m; i++) {
-                int[] data = Arrays.stream(br.readLine().split("\\s+"))
+
+            for (int bus = 0; bus < m; bus++) {
+                int[] tokens = Arrays.stream(br.readLine().split("\\s+"))
                         .mapToInt(Integer::parseInt)
                         .toArray();
-                int from = data[0] - 1, to = data[1] - 1, cost = data[2];
+                int from = tokens[0] - 1;
+                int to = tokens[1] - 1;
+                int cost = tokens[2];
 
-            priceList[from][to] = Math.min(priceList[from][to], cost);
+                distance[from][to] = Math.min(distance[from][to], cost);
             }
 
-            for (int baseArea = 0; baseArea < n; baseArea++) {
+            // 플로이드 와샬
+            for (int via = 0; via < n; via++) {
                 for (int from = 0; from < n; from++) {
                     for (int to = 0; to < n; to++) {
-                        if (priceList[from][baseArea] != Integer.MAX_VALUE &&priceList[baseArea][to] != Integer.MAX_VALUE) {
-                        priceList[from][to] = Math.min(priceList[from][to],priceList[from][baseArea] +priceList[baseArea][to]);
-                        }
+                        if (from == to)
+                            continue;
+
+                        if (distance[from][via] == Integer.MAX_VALUE || distance[via][to] == Integer.MAX_VALUE)
+                            continue;
+
+                        distance[from][to] = Math.min(distance[from][to], distance[from][via] + distance[via][to]);
                     }
                 }
             }
 
-             Arrays.stream(priceList)
-                    .forEach(row -> {
-                        Arrays.stream(row).forEach(col -> System.out.print((col == Integer.MAX_VALUE ? 0 : col) + " "));
-                        System.out.println();
-                    });
-        } catch (Exception e) {
-            System.out.println("error");
-        }
+            StringBuilder answer;
+            for (int from = 0; from < n; from++) {
+                answer = new StringBuilder();
+                for (int to = 0; to < n; to++) {
+                    if (distance[from][to] == Integer.MAX_VALUE) // 갈 수 없는 경우
+                        answer.append(0).append(" ");
+                    else
+                        answer.append(distance[from][to]).append(" ");
+                }
+                bw.write(answer.toString().trim() + "\n");
+            }
 
+            // 출력
+            bw.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
