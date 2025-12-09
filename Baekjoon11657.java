baec89f7d79e91499c653a9151e1a3cfb4cf61a5
Pangtaek <zombie3632@gmail.com>
2025-12-09 19:30:49 +0900

    타임머신

--- Baekjoon11657.java
+++ Baekjoon11657.java
@@ -1,76 +1,82 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Baekjoon11657 {
 
-    static class Edge {
-        int from, to, time;
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
 
-        public Edge(int from, int to, int time) {
-            this.from = from;
-            this.to = to;
-            this.time = time;
-        }
-    }
+            int[] tokens = Arrays.stream(br.readLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            int N = tokens[0]; // 정점의 개수
+            int M = tokens[1]; // 간선의 개수
 
-    static int N, M;
-    static Edge[] edges;
-    static final long INF = Long.MAX_VALUE;
-    static long[] dist;
+            int[][] edges = new int[M][3];
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < M; i++) {
+                tokens = Arrays.stream(br.readLine().split("\\s+"))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+                int from = tokens[0];
+                int to = tokens[1];
+                int cost = tokens[2];
 
-        N = Integer.parseInt(st.nextToken()); // 정점 수
-        M = Integer.parseInt(st.nextToken()); // 간선 수
-
-        edges = new Edge[M];
-        dist = new long[N + 1];
-
-        for (int i = 1; i <= N; i++) {
-            dist[i] = INF;
-        }
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int time = Integer.parseInt(st.nextToken());
-
-            edges[i] = new Edge(from, to, time);
-        }
-
-        dist[1] = 0; // 1번 정점 시작
-
-        // 벨만-포드 알고리즘
-        if (bellmanFord()) {
-            System.out.println(-1); // 음수 사이클 존재
-        } else {
-            for (int i = 2; i <= N; i++) {
-                System.out.println(dist[i] == INF ? -1 : dist[i]);
+                edges[i][0] = from;
+                edges[i][1] = to;
+                edges[i][2] = cost;
             }
-        }
-    }
 
-    public static boolean bellmanFord() {
-        for (int i = 1; i <= N - 1; i++) {
-            for (Edge e : edges) {
-                if (dist[e.from] != INF && dist[e.to] > dist[e.from] + e.time) {
-                    dist[e.to] = dist[e.from] + e.time;
+            long[] distance = new long[N + 1];
+            Arrays.fill(distance, Integer.MAX_VALUE);
+            distance[1] = 0;
+
+            for (int i = 1; i <= N; i++) {
+                for (int[] edge : edges) {
+                    int from = edge[0];
+                    int to = edge[1];
+                    int cost = edge[2];
+
+                    if (distance[from] == Integer.MAX_VALUE)
+                        continue;
+
+                    if (distance[to] > distance[from] + cost) {
+                        distance[to] = distance[from] + cost;
+                    }
                 }
             }
-        }
 
-        // N번째 반복에서 갱신이 일어나면 음수 사이클 존재
-        for (Edge e : edges) {
-            if (dist[e.from] != INF && dist[e.to] > dist[e.from] + e.time) {
-                return true;
+            boolean isFlag = true;
+
+            for (int[] edge : edges) {
+                int from = edge[0];
+                int to = edge[1];
+                int cost = edge[2];
+
+                if (distance[from] == Integer.MAX_VALUE)
+                    continue;
+
+                if (distance[to] > distance[from] + cost) {
+                    isFlag = false;
+                    break;
+                }
             }
-        }
 
-        return false;
+            if (isFlag) {
+                for (int i = 2; i <= N; i++) {
+                    bw.write(String.valueOf(distance[i] == Integer.MAX_VALUE ? -1 : distance[i]) + "\n");
+                }
+            } else {
+                bw.write("-1\n");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
